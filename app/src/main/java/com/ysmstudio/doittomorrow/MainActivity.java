efd20e5b85d071f534c0d7513782cb4379b3a814
Seungmin Yang <tmdals099@gmail.com>
2020-01-07 23:18:04 +0900

    Illustration icon 추가, MainActivity 기본 레이아웃 구성, Recycler view adapter 뼈대

--- app/src/main/java/com/ysmstudio/doittomorrow/MainActivity.java
+++ app/src/main/java/com/ysmstudio/doittomorrow/MainActivity.java
@@ -1,14 +1,26 @@
 package com.ysmstudio.doittomorrow;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import com.ysmstudio.doittomorrow.databinding.ActivityMainBinding;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setActivity(this);
+
+        setSupportActionBar(binding.toolbar);
+    }
+
+    public void onFabClick(View view) {
+
     }
 }
---
+++ app/src/main/java/com/ysmstudio/doittomorrow/TodoRecyclerViewAdapter.java
@@ -0,0 +1,31 @@
+package com.ysmstudio.doittomorrow;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TodoRecyclerViewAdapter extends RecyclerView.Adapter<TodoRecyclerViewAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
