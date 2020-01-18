5cc2eec9811b51dfb1140c3cbcf18fc6786f1766
Seungmin Yang <tmdals099@gmail.com>
2020-01-19 04:04:40 +0900

    BroadcastReceiver 등록

--- app/src/main/java/com/ysmstudio/doittomorrow/DoItTomorrow.java
+++ app/src/main/java/com/ysmstudio/doittomorrow/DoItTomorrow.java
@@ -1,13 +1,29 @@
 package com.ysmstudio.doittomorrow;
 
 import android.app.Application;
+import android.app.PendingIntent;
+import android.content.Intent;
 
 import io.realm.Realm;
 
 public class DoItTomorrow extends Application {
+    public static final int REQ_TODAY_TODO_PENDING = 100;
+
     @Override
     public void onCreate() {
         super.onCreate();
         Realm.init(this);
+        createTodayTodoNotiPendingIntent();
+    }
+
+    private void createTodayTodoNotiPendingIntent() {
+        Intent intent = new Intent("com.ysmstudio.doittomorrow.RESET_ALARM");
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                this,
+                REQ_TODAY_TODO_PENDING,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
     }
 }
---
+++ app/src/main/java/com/ysmstudio/doittomorrow/TodayTodoNotiReceiver.java
@@ -0,0 +1,15 @@
+package com.ysmstudio.doittomorrow;
+
+import android.app.Notification;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class TodayTodoNotiReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+    }
+}
