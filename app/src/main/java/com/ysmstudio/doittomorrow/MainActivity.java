b81179371d005b8e2b5aa48876b842c181cb0061
Seungmin Yang <tmdals099@gmail.com>
2020-01-19 01:39:37 +0900

    뷰 위치 조정 및 클릭 리스너 추가

--- app/src/main/java/com/ysmstudio/doittomorrow/MainActivity.java
+++ app/src/main/java/com/ysmstudio/doittomorrow/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
     private void setRecyclerView() {
         adapter = new TodoRecyclerViewAdapter();
         adapter.setList(new ArrayList<TodoData>());
-        adapter.setEmptyView(binding.content.recyclerEmptyView);
+        adapter.setEmptyView(binding.content.recyclerEmptyView.getRoot());
         binding.content.recyclerViewTodoTomorrow.setLayoutManager(
                 new LinearLayoutManager(this)
         );
@@ -73,5 +73,9 @@ public class MainActivity extends AppCompatActivity {
         dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
         dialog.show();
         inflate.findViewById(R.id.edit_text_name).requestFocus();
+    }
+
+    public void onViewPreviousTodoClock(View view) {
+        // TODO: 2020-01-19 previous activity 이동
     }
 }
