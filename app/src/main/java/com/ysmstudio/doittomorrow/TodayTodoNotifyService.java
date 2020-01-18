de8f61ba4fd5de230003c888212abd1e848b761a
Seungmin Yang <tmdals099@gmail.com>
2020-01-19 02:11:35 +0900

    service 추가

---
+++ app/src/main/java/com/ysmstudio/doittomorrow/TodayTodoNotifyService.java
@@ -0,0 +1,16 @@
+package com.ysmstudio.doittomorrow;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class TodayTodoNotifyService extends Service {
+    public TodayTodoNotifyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
