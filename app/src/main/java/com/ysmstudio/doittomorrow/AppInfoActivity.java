7f26604abbe6ee39e8402d74f8608374c2ecd31f
Seungmin Yang <tmdals099@gmail.com>
2020-01-27 00:15:39 +0900

    App info Activity 추가

---
+++ app/src/main/java/com/ysmstudio/doittomorrow/AppInfoActivity.java
@@ -0,0 +1,14 @@
+package com.ysmstudio.doittomorrow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AppInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_info);
+    }
+}
